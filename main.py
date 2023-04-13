7d22c3a7a74b637f86950c8cc5215877b337abf5
draginsky <cronosdversus@mail.ru>
2023-04-13 23:39:46 +0500

    Base main

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from googleDrive.google_drive import upload_file
+import yandexdisk.yandextest
 
 
 def main():
-    upload_file('googleDrive/Hello.txt')
-    upload_file('googleDrive/SCR-20230320-qiji.png')
+    drive_choice = input()
+    if drive_choice == 'google':
+        upload_file('googleDrive/Hello.txt')
+        upload_file('googleDrive/SCR-20230320-qiji.png')
+    elif drive_choice == 'yandex':
+        pass
 
 
 if __name__ == '__main__':
