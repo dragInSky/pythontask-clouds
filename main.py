82c319d894cf3631d2eb5a257b979989bcf2afa2
draginsky <cronosdversus@mail.ru>
2023-04-13 23:37:28 +0500

    Fix structure issue

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from googleDrive.google_drive import upload_file
 
 
 def main():
-    upload_file('Hello.txt')
+    upload_file('googleDrive/Hello.txt')
+    upload_file('googleDrive/SCR-20230320-qiji.png')
 
 
 if __name__ == '__main__':
