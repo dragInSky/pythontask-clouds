f0e588fcab01cd344638368d39691fee9e520f2f
draginsky <cronosdversus@mail.ru>
2023-04-14 19:37:11 +0500

    Right merge yandex and google branches

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from googleDrive.google_drive import upload_file
-from yandexdisk.yandextest import upload_file, get_user_token, backup
 import config
-from googleDrive.google_drive import upload_folder
 import yandexdisk.yandextest
 import argparse
+
+from googleDrive.google_drive import upload_file
+from yandexdisk.yandextest import upload_file, get_user_token, backup
+from googleDrive.google_drive import upload_folder
 
 
 def main():
@@ -21,13 +22,17 @@ def main():
         help='choosing a yandex disk cloud'
     )
     args = parser.parse_args()
+
     if args.googleDrive:
+        print('google')
         upload_file('/Users/draginsky/PycharmProjects/pythontask-clouds/googleDrive/Hello.txt', 'root')
         upload_file('/Users/draginsky/PycharmProjects/pythontask-clouds/googleDrive/SCR-20230320-qiji.png', 'root')
 
-        # upload_folder('/Users/draginsky/PycharmProjects/pythontask-clouds/yandexdisk')
+        upload_folder('/Users/draginsky/PycharmProjects/pythontask-clouds/yandexdisk')
     elif args.yandexDisk:
-        pass
+        print('yandex')
+        user_code = get_user_token()
+        backup(user_code, "Тестовая папка для тестового бэкапа", "C:\\pythontask-clouds")
 
 
 if __name__ == '__main__':
