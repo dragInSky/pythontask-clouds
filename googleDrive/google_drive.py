02d69030d5ab24588a08efa92a71b192f669312d
draginsky <cronosdversus@mail.ru>
2023-04-16 11:17:25 +0500

    Add pretty file listing Add file downloading

--- googleDrive/google_drive.py
+++ googleDrive/google_drive.py
@@ -2,13 +2,15 @@ from __future__ import print_function
 
 import mimetypes
 import os.path
+import io
 
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from tabulate import tabulate
 
 
 class GoogleDrive:
@@ -44,6 +46,7 @@ class GoogleDrive:
 
             media = MediaFileUpload(file_fullpath, mimetype=mimetypes.MimeTypes().guess_type(file_fullpath)[0])
             self._service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+
         except HttpError as e:
             print(f'Exception: {e}')
 
@@ -73,138 +76,46 @@ class GoogleDrive:
                 for name in filenames:
                     file_fullpath = os.path.join(dir_path, name)
                     self.upload_file(file_fullpath, folder_id)
+
         except HttpError as e:
             print(f'Exception: {e}')
 
+    def file_listing(self):
+        try:
+            results = self._service.files().list(
+                fields="nextPageToken, files(id, name, size, modifiedTime)"
+            ).execute()
+            items = results.get('files', [])
 
-# SCOPES = ['https://www.googleapis.com/auth/drive']
-#
-#
-# def get_credentials() -> Credentials:
-#     creds = None
-#     if os.path.exists('googleDrive/token.json'):
-#         creds = Credentials.from_authorized_user_file('googleDrive/token.json', SCOPES)
-#     if not creds or not creds.valid:
-#         if creds and creds.expired and creds.refresh_token:
-#             creds.refresh(Request())
-#         else:
-#             flow = InstalledAppFlow.from_client_secrets_file('googleDrive/credentials.json', SCOPES)
-#             creds = flow.run_local_server(port=0)
-#         with open('googleDrive/token.json', 'w') as token:
-#             token.write(creds.to_json())
-#     return creds
-#
-#
-# def upload_file(file_fullpath: str, folder_id: str = 'root'):
-#     name = file_fullpath.split('/')[-1]
-#     file_path = file_fullpath.removesuffix(name)
-#     _upload_file(file_path, name, folder_id)
-#
-#
-# def _upload_file(file_path: str, name: str, folder_id: str):
-#     try:
-#         creds = get_credentials()
-#         service = build('drive', 'v3', credentials=creds)
-#
-#         file_metadata = {
-#             'name': name,
-#             'parents': [folder_id]
-#         }
-#
-#         file_fullpath = os.path.join(file_path, name)
-#         media = MediaFileUpload(file_fullpath, mimetype=mimetypes.MimeTypes().guess_type(file_fullpath)[0])
-#         service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-#     except HttpError as e:
-#         print(f'Exception: {e}')
-#
-#
-# def _inner_upload_folder(last_dir: str, pre_last_dir: str, parents_id: dict[str, Any], root: str, files: list):
-#     creds = get_credentials()
-#     service = build('drive', 'v3', credentials=creds)
-#
-#     folder_metadata = {
-#         'name': last_dir,
-#         'parents': [pre_last_dir],
-#         'mimeType': 'application/vnd.google-apps.folder'
-#     }
-#
-#     create_folder = service.files().create(body=folder_metadata, fields='id').execute()
-#     folder_id = create_folder.get('id', [])
-#     parents_id[last_dir] = folder_id
-#
-#     for name in files:
-#         _upload_file(root, name, folder_id)
-#
-#
-# def upload_folder(folder_path: str):
-#     try:
-#         parents_id = {}
-#
-#         for root, _, files in os.walk(folder_path, topdown=True):
-#             print(root, _, files)
-#             last_dir = root.split('/')[-1]
-#             pre_last_dir = root.split('/')[-2]
-#             if pre_last_dir not in parents_id.keys():
-#                 pre_last_dir = []
-#             else:
-#                 pre_last_dir = parents_id[pre_last_dir]
-#
-#             _inner_upload_folder(last_dir, pre_last_dir, parents_id, root, files)
-#     except HttpError as e:
-#         print(f'Exception: {e}')
-#
-# # def get_os_tree(folder_path: str, os_tree: list):
-# #     root_len = len(folder_path.split(os.path.sep)[0:-2])
-# #     for root, dirs, files in os.walk(folder_path, topdown=True):
-# #         for name in dirs:
-# #             var_path = '/'.join(root.split('/')[root_len + 1:])
-# #             os_tree.append(os.path.join(var_path, name))
-# #
-# #
-# # def get_cloud_tree(folder_name: str, root: str, parents_id: dict[str, Any], cloud_tree: list):
-# #     try:
-# #         creds = get_credentials()
-# #         service = build('drive', 'v3', credentials=creds)
-# #
-# #         folder_id = parents_id[folder_name]
-# #
-# #         results = service.files().list(
-# #             pageSize=100,
-# #             q=("%r in parents and mimeType = \
-# #             'application/vnd.google-apps.folder'and trashed != True" % folder_id)
-# #         ).execute()
-# #
-# #         items = results.get('files', [])
-# #         root += folder_name + os.path.sep
-# #
-# #         for item in items:
-# #             parents_id[item['name']] = item['id']
-# #             cloud_tree.append(root + item['name'])
-# #             folder_name = item['name']
-# #             get_cloud_tree(folder_name, root, parents_id, cloud_tree)
-# #     except HttpError as e:
-# #         print(f'Exception: {e}')
-# #
-# #
-# # def folders_sync(folder_path: str, upload_folders: list, parents_id: dict[str, Any]):
-# #     for folder_dir in upload_folders:
-# #         var = os.path.join(folder_path.split(os.path.sep)[-1]) + os.path.sep
-# #         root = var + folder_dir
-# #         last_dir = folder_dir.split(os.path.sep)[-1]
-# #         pre_last_dir = folder_dir.split(os.path.sep)[-2]
-# #
-# #         files = [f for f in os.listdir(root) if os.path.isfile(os.path.join(root, f))]
-# #
-# #         _inner_upload_folder(last_dir, pre_last_dir, parents_id, root, files)
-# #
-# #
-# # def pc_cloud_sync(folder_path: str, folder_name: str, root: str, parents_id):
-# #     os_tree, cloud_tree = [], []
-# #     get_os_tree(folder_name, os_tree)
-# #     get_cloud_tree(folder_name, root, parents_id, cloud_tree)
-# #     remove_folders = list(set(cloud_tree).difference(set(os_tree)))
-# #     upload_folders = list(set(os_tree).difference(set(cloud_tree)))
-# #     exact_folders = list(set(os_tree).intersection(set(cloud_tree)))
-# #
-# #     upload_folders = sorted(upload_folders)
-# #     folders_sync(folder_path, upload_folders, parents_id)
+            if not items:
+                print('No files found.')
+            else:
+                output = list(list())
+                for item in items:
+                    size = 'folder' if 'size' not in item else item['size']
+                    modified_time = item['modifiedTime'].replace('T', ' ')
+                    modified_time = modified_time[:modified_time.index('.')]
+                    row = [
+                        '{0}'.format(item['name']),
+                        '{0}'.format(item['id']),
+                        '{0}'.format(size),
+                        '{0}'.format(modified_time)
+                    ]
+                    output.append(row)
+                print(tabulate(output, headers=['name', 'id', 'size', 'last modify time']))
+
+        except HttpError as e:
+            print(f'Exception: {e}')
+
+    def download_file(self, download_dir: str, filename: str, file_id: str):
+        try:
+            request = self._service.files().get_media(fileId=file_id)
+            file = io.FileIO(os.path.join(download_dir, filename), 'wb')
+            downloader = MediaIoBaseDownload(file, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print(F'Download {int(status.progress() * 100)}.')
+
+        except HttpError as e:
+            print(f'Exception: {e}')
--- main.py
+++ main.py
@@ -24,11 +24,23 @@ def main():
 
     if args.googleDrive:
         print('google')
-        GD = GoogleDrive()
-        GD.upload_file('/Users/draginsky/PycharmProjects/pythontask-clouds/googleDrive/Hello.txt')
-        GD.upload_file('/Users/draginsky/PycharmProjects/pythontask-clouds/googleDrive/SCR-20230320-qiji.png')
 
-        GD.upload_folder('/Users/draginsky/PycharmProjects/pythontask-clouds/yandexdisk')
+        # file_path = '\\Users\\draginsky\\PycharmProjects\\pythontask-clouds\\googleDrive\\Hello.txt'
+        folder_path = '/Users/draginsky/PycharmProjects/pythontask-clouds/download'
+
+        # file_path = file_path.replace('\\', '/')
+        folder_path = folder_path.replace('\\', '/')
+
+        GD = GoogleDrive()
+        GD.file_listing()
+
+        GD.download_file(
+            '/Users/draginsky/PycharmProjects/pythontask-clouds/download',
+            'SCR-20230320-qiji.png',
+            '1VGUohkQ951DLIeD2yJQJVgWsBtdV8idg'
+        )
+        # GD.upload_file(file_path)
+        # GD.upload_folder(folder_path)
     elif args.yandexDisk:
         print('yandex')
         user_code = get_user_token()
